bad8ed1463a8708293c1e95331571ab44ff7f015
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:58:19 +0200

    Delete synthese.sh

--- scripts/wavenet/synthese.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/deepvoice3_by_tg
-
-#python synthesis.py /lium/raid01_b/tgranjon/dv3/checkpoints/checkpoint_step$1.pth tests/test_fr/roman.txt /lium/raid01_b/tgranjon/dv3/synthesis --preset=presets/deepvoice3_synpaflex.json --checkpoint_wavenet=/lium/raid01_b/tgranjon/wavenet/checkpoints/checkpoint_step$1.pth
-python synthesis.py /lium/raid01_b/tgranjon/dv3/checkpoints/average.pth tests/test_fr/roman.txt /lium/raid01_b/tgranjon/dv3/synthesis --preset=presets/deepvoice3_synpaflex.json --checkpoint-wavenet=/lium/raid01_b/tgranjon/wavenet/checkpoints/checkpoint_step$1.pth
-
-cd ~/scripts/dv3
