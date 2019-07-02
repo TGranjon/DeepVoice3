aaa9f78e5d9892f9b0e5a84f0c69efacecf7b7c8
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:36:55 +0200

    Update preprocess.sh

--- scripts/dv3/world/preprocess.sh
+++ scripts/dv3/world/preprocess.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --mem 45G
-#SBATCH -c 2
-#SBATCH --job-name Preprocess-DV3-World
-#SBATCH --time 02:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=thomas.granjon.etu@univ-lemans.fr
 
-python ~/deepvoice3_by_tg/preprocess.py synpaflex /lium/raid01_b/tgranjon/synpaflex/full /lium/raid01_b/tgranjon/dv3/world/data --preset=../../../deepvoice3_by_tg/presets/deepvoice3_synpaflex.json
+# Effectue le prétraitement.
+
+cd ../../../deepvoice3_world/
+
+# python preprocess.py <nom du corpus> <emplacement du fichier CSV qui contient les données> <emplacement où sauvegarder les données pré-traitées> --preset=<preset>
+
+python preprocess.py synpaflex /lium/raid01_b/tgranjon/synpaflex/full /lium/raid01_b/tgranjon/dv3/world/data --preset=../../../deepvoice3_by_tg/presets/deepvoice3_synpaflex.json
+
+cd ../scripts/dv3/world
