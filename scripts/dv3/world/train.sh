09e933e44954a5bea407605294879079aa1e3f10
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:38:18 +0200

    Update train.sh

--- scripts/dv3/world/train.sh
+++ scripts/dv3/world/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -c2
-#SBATCH --mem 40G
-#SBATCH -p gpu
-#SBATCH --job-name train-DV3-World
-#SBATCH --gres gpu:1
-#SBATCH --time 08-00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=thomas.granjon.etu@univ-lemans.fr
 
-python ~/deepvoice3_by_tg/train.py --preset=../../../deepvoice3_by_tg/presets/deepvoice3_synpaflex.json --data-root=/lium/raid01_b/tgranjon/dv3/world/data --checkpoint-dir=/lium/raid01_b/tgranjon/dv3/world/checkpoints --log-event-path=/lium/raid01_b/tgranjon/dv3/world/logs
+# Lance l'entraînement.
+
+cd ../../../deepvoice3_world
+
+# python train.py --preset=<preset> --data-root=<emplacement des données pré-traitées> --checkpoint-dir=<où sauvegarder les checkpoints> --log-event-path=<où sauvegarder les logs> 
+
+python train.py --preset=deepvoice3_by_tg/presets/deepvoice3_synpaflex.json --data-root=/lium/raid01_b/tgranjon/dv3/world/data --checkpoint-dir=/lium/raid01_b/tgranjon/dv3/world/checkpoints --log-event-path=/lium/raid01_b/tgranjon/dv3/world/logs
+
+cd ../scripts/dv3/world
