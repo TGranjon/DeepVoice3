2bc65169749c8cf93c66c0cc09563a09ec05f1ff
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:27:25 +0200

    Update preprocess.sh

--- scripts/dv3/preprocess.sh
+++ scripts/dv3/preprocess.sh
@@ -4,7 +4,7 @@
 
 cd ../../deepvoice3_by_tg/
 
-# python preprocess.py <nom du corpus> <emplacement du fichier CSV qui contient les données> <emplacement où sauvegarder les données pré-traitées> <preset>
+# python preprocess.py <nom du corpus> <emplacement du fichier CSV qui contient les données> <emplacement où sauvegarder les données pré-traitées> --preset=<preset>
 python preprocess.py synpaflex /lium/raid01_b/tgranjon/synpaflex/full /lium/raid01_b/tgranjon/dv3/data --preset=presets/deepvoice3_synpaflex.json
 
 cd ../scripts/dv3/
