077fa0df5948f9fb9aec6fb51d139f954f059709
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:11:57 +0200

    Update checkpoint.sh

--- scripts/dv3/checkpoint.sh
+++ scripts/dv3/checkpoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#SBATCH -c2
-#SBATCH --mem 120G
-#SBATCH -p gpu
-#SBATCH --gres gpu:k40:1
-#SBATCH --time 08-00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=thomas.granjon.etu@univ-lemans.fr
 
-python ~/deepvoice3_pytorch/train.py --preset=../../deepvoice3_pytorch/presets/deepvoice3_synpaflex.json --data-root=/lium/raid01_b/tgranjon/dv3/data --checkpoint-dir=/lium/raid01_b/tgranjon/dv3/checkpoints --log-event-path=/lium/raid01_b/tgranjon/dv3/logs --restore-parts=/lium/raid01_b/tgranjon/dv3/checkpoints/checkpoint_step000570000.pth
+# Lance l'apprenstissage en partant d'un checkpoint spécifié.
+# python <script train.py> <preset> <emplacement des données pré-traitées> <emplacement où sauvegarder les checkpoints> <emplacement où sauvegarder les logs> <emplacement du checkpoint duquel partir>
+python ../../deepvoice3_by_tg/train.py --preset=../../deepvoice3_by_tg/presets/deepvoice3_synpaflex.json --data-root=/lium/raid01_b/tgranjon/dv3/data --checkpoint-dir=/lium/raid01_b/tgranjon/dv3/checkpoints --log-event-path=/lium/raid01_b/tgranjon/dv3/logs --restore-parts=/lium/raid01_b/tgranjon/dv3/checkpoints/checkpoint_step000570000.pth
