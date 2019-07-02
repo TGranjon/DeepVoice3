ef3c02d4e7d4b984ebbfaef1ce6a56a28b0ead6d
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:49:19 +0200

    Update train.sh

--- scripts/tacotron/train.sh
+++ scripts/tacotron/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -c2
-#SBATCH --mem 100G
-#SBATCH -p gpu
-#SBATCH --job-name train-Tacotron-Fr
-#SBATCH --gres gpu:4
-#SBATCH --time 06-00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=thomas.granjon.etu@univ-lemans.fr
 
-#python ~/tacotron_pytorch/train.py --data-root=/lium/raid01_b/tgranjon/tacotron/data --checkpoint-dir=/lium/raid01_b/tgranjon/tacotron/checkpoints
-python ~/tacotron_pytorch/train.py --data-root=/lium/raid01_b/tgranjon/tacotron/data/fr --checkpoint-dir=/lium/raid01_b/tgranjon/tacotron/checkpoints/fr
+cd ../../tacotron/
+
+# python train.py --data-root=<emplacement des données pré-traitées> --checkpoint-dir=<emplacement où sauvegarder les checkpoints> 
+# Version en anglais.
+python train.py --data-root=/lium/raid01_b/tgranjon/tacotron/data --checkpoint-dir=/lium/raid01_b/tgranjon/tacotron/checkpoints
+
+# Version en français (_ne fonctionne pas pour l'instant_)
+#python train.py --data-root=/lium/raid01_b/tgranjon/tacotron/data/fr --checkpoint-dir=/lium/raid01_b/tgranjon/tacotron/checkpoints/fr
+
+cd ../scripts/tacotron
