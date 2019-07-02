18687f9059385faf9882968159ef64c77943e3c3
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:57:42 +0200

    Update preprocess.sh

--- scripts/wavenet/preprocess.sh
+++ scripts/wavenet/preprocess.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-#SBATCH --mem 45G
-#SBATCH -p gpu
-#SBATCH --gres gpu:1
-#SBATCH -c 2
-#SBATCH --job-name Preprocess-Wavenet
-#SBATCH --time 02:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=thomas.granjon.etu@univ-lemans.fr
 
-cd ~/wavenet_vocoder
-#python preprocess.py synpaflex /lium/raid01_b/tgranjon/synpaflex/full /lium/raid01_b/tgranjon/wavenet/data --preset=presets/synpaflex_gaussian.json
-python preprocess.py synpaflex /lium/raid01_b/tgranjon/synpaflex /lium/raid01_b/tgranjon/wavenet/data --preset=presets/synpaflex_mixture.json
-cd ~/scripts/wavenet
+# Lance le pré-traitement des données.
+
+cd ../../wavenet_vocoder
+
+# python preprocess.py <nom du corpus> <adresse du fichier CSV> <Adresse où sauvegarder les données> --preset=<preset>
+python preprocess.py synpaflex /lium/raid01_b/tgranjon/synpaflex/full /lium/raid01_b/tgranjon/wavenet/data --preset=presets/synpaflex_gaussian.json
+
+cd ../scripts/wavenet
