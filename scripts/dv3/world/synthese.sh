a68c3fafa682093be2cd54cdcff5e03dcd7d11c3
Thomas GRANJON <thomas.granjon@orange.fr>
2019-07-02 14:37:36 +0200

    Update synthese.sh

--- scripts/dv3/world/synthese.sh
+++ scripts/dv3/world/synthese.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd ~/deepvoice3_by_tg
+cd ../../../deepvoice3_world
 
+# python synthesis.py <adresse du checkpoint> <adresse du fichier texte à synthétiser> <emplacement où sauvegarder les fichiers audio> --preset=<preset>
 python synthesis.py /lium/raid01_b/tgranjon/dv3/world/checkpoints/checkpoint_step$1.pth ~/deepvoice3_by_tg/tests/test_fr/text_fr.txt /lium/raid01_b/tgranjon/dv3/world/synthesis --preset=presets/deepvoice_synpaflex.json
 
 
-cd ~/scripts/dv3
+cd ../scripts/dv3/world
